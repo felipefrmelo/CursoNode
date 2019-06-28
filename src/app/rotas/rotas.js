99247017b8437c9bbf7e7363a84a190fd2ba7671
felipefrmelo <felipefrmelo@gmail.com>
2019-06-28 17:25:00 -0300

    MVC, autenticação e autorização

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,34 +1,7 @@
-const LivroDao = require('../infra/livro-dao')
-const db = require('../../config/database')
+const livroRotas = require('./livro-rotas');
+const baseRotas = require('./base-rotas');
 
 module.exports = (app) => {
-    app.get('/', function (req, resp) {
-        resp.send(
-            `
-                <html>
-                    <head>
-                        <meta charset="utf-8">
-                    </head>
-                    <body>
-                        <h1> Casa do Código </h1>
-                    </body>
-                </html>
-            `
-        );
-    });
-
-    app.get('/livros', function (req, resp) {
-
-        const livroDao = new LivroDao(db)
-
-        livroDao.lista()
-            .then((response) => resp.marko(
-                require('../views/livros/lista/lista.marko'),
-                {
-                    livros: response
-                }
-            )
-            )
-            .catch(erro => console.log(erro));
-    });
+    baseRotas(app);
+    livroRotas(app);
 };
